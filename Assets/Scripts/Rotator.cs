287bb7166f6e80cbfa9ca549d0583789a6f78de2
Nikita Krutoy <nikitakrutoy@gmail.com>
2020-11-07 06:00:57 +0300

    Added CylinderWolrd example

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rate = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, rate * Time.deltaTime, Space.World);
+    }
+}
