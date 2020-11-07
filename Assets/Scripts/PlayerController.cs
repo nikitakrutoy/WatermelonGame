cbd16457b6ba167ab2551a12b6ef7bca2080cdd9
valeria <urgbal@gmail.com>
2020-11-07 11:45:29 +0200

    platform generator added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 5f;
-    [SerializeField] private GameObject ceilSky;
     private float jumpHeight = 10f;
-    private float skyHeight;
     private Rigidbody rb;
 
 
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        skyHeight = ceilSky.transform.position.y;
     }
 
     private void Update()
@@ -23,7 +20,6 @@ public class PlayerController : MonoBehaviour
         float yMovement = Input.GetAxis("Vertical") * movementSpeed;
 
         transform.Translate(new Vector3(0, 0, yMovement) * Time.deltaTime);
-        ceilSky.transform.position = new Vector3(transform.position.x, skyHeight, transform.position.z);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
